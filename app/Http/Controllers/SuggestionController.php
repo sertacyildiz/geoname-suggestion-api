9aff465fa6e148469ba20fc40e61bf35c8bcbb5e
SERTAC\Sertac <sertac@vidobu.com>
2020-12-01 18:47:56 +0300

    Update SuggestionController.php

--- app/Http/Controllers/SuggestionController.php
+++ app/Http/Controllers/SuggestionController.php
@@ -6,7 +6,6 @@ use App\Http\Requests\GetSuggestions;
 use App\Repositories\SuggestionRepository;
 use App\Rules\Latitude;
 use App\Rules\Longitude;
-use App\Services\SuggestionService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Validation\ValidationException;
